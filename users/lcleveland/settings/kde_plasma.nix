59799d8f3915394cc896d9ea8f6c0f8339199888
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-27 18:21:46 -0500

    add wallpaper

--- users/lcleveland/settings/kde_plasma.nix
+++ users/lcleveland/settings/kde_plasma.nix
@@ -9,6 +9,7 @@
         enable = true;
         workspace = {
           lookAndFeel = "org.kde.breezedark.desktop";
+          wallpaper = "../../../resources/wallpapers/sysmind_brain_green.png";
         };
       };
     };
