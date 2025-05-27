c9c6cb62d5cab44d2babbc2319eee69cbe69041d
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-27 17:37:07 -0500

    add plasma manager input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Reusable NixOS user configurations";
-  outputs = { self, home-manager, nixpkgs, zen_browser }@inputs:
+  outputs = { self, home-manager, nixpkgs, plasma_manager, zen_browser }@inputs:
     let
       import_modules = import ./import_modules.nix;
       users = import_modules ./users;
@@ -10,6 +10,11 @@
       nixosModules.default = users;
     };
   inputs = {
+    plasma_manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
     zen_browser = {
       url = "github:0xc000022070/zen-browser-flake";
     };
