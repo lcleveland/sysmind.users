f287fb2ddf42399735e157ef1ced986ca7200336
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-27 17:49:28 -0500

    test

--- users/lcleveland/settings/kde_plasma.nix
+++ users/lcleveland/settings/kde_plasma.nix
@@ -1,9 +1,9 @@
-{ config, lib, ... }:
+{ config, lib, plasma_manager, ... }:
 {
   config = {
     home-manager = {
       sharedModules = [
-        plasma-manager.homeManagerModules.plasma-manager
+        plasma_manager.plasma-manager.homeManagerModules.plasma-manager
       ];
       users.lcleveland.programs.plasma = {
         enable = true;
