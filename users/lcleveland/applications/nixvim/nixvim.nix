5ac48ba4953d652aae523f4ddf50e65e8f843eca
Lyle Cleveland <lcleveland@proton.me>
2025-05-28 10:44:56 -0400

    add format

--- users/lcleveland/applications/nixvim/nixvim.nix
+++ users/lcleveland/applications/nixvim/nixvim.nix
@@ -11,6 +11,11 @@
           cmp = {
             enable = true;
             autoEnableSources = true;
+	    settings = {
+	    	sources = [
+		    { name = "nvim_lsp"; }
+		    ];
+	    };
           };
           cmp-nvim-lsp.enable = true;
           lsp = {
@@ -27,6 +32,10 @@
             };
           };
           lsp-format.enable = true;
+	  none-ls = {
+	  	enable = true;
+		sources.formatting.nixpkgs_fmt.enable = true;
+		};
           supermaven.enable = true;
           treesitter = {
             enable = true;
