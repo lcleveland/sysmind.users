f1815aa1a6e9715256246a7850de5b3d145bf235
Lyle Cleveland <lcleveland@proton.me>
2025-05-28 14:50:23 -0400

    test title

--- users/lcleveland/settings/kde_plasma.nix
+++ users/lcleveland/settings/kde_plasma.nix
@@ -17,6 +17,9 @@
                 };
               }
               {
+                applicationTitleBar = { };
+              }
+              {
                 panelSpacer = {
                   expanding = true;
                 };
