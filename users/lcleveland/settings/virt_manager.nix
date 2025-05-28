abba0838b2551232d7b89f8d9f524ad6663fab47
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-27 19:20:06 -0500

    add virt manager settings

---
+++ users/lcleveland/settings/virt_manager.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+{
+  config = {
+    home-manager.users.lcleveland.dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+}
