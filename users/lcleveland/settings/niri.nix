f5eb579686671d953a3308b522cbaec3feb08e84
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 18:56:29 -0500

    test niri

---
+++ users/lcleveland/settings/niri.nix
@@ -0,0 +1,11 @@
+{ config, lib, inputs, ... }:
+{
+  config = {
+    home-manager.users.lcleveland.programs.niri = {
+      enable = true;
+      settings.binds = {
+        "Mod+T".action.spawn = "ghostty";
+      };
+    };
+  };
+}
