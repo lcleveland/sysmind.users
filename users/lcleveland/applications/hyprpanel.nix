8cee0df72a6105adc519b917cf2149a72138d5c1
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 21:17:10 -0500

    enable

--- users/lcleveland/applications/hyprpanel.nix
+++ users/lcleveland/applications/hyprpanel.nix
@@ -1,9 +1,14 @@
 { config, lib, pkgs, inputs, ... }:
 {
   config = {
-    home-manager.users.lcleveland.home.packages = with pkgs; [
-      inputs.hyprpanel.packages.${pkgs.system}.wrapper
-      hyprpanel
-    ];
+    home-manager.users.lcleveland = {
+      home.packages = with pkgs; [
+        inputs.hyprpanel.packages.${pkgs.system}.wrapper
+        hyprpanel
+      ];
+      programs.hyprpanel = {
+        enable = true;
+      };
+    };
   };
 }
