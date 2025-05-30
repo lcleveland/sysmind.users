9b6086a4a186ce6cdc697a5a870cad884f9c1e71
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 19:49:16 -0500

    test

--- users/lcleveland/settings/niri.nix
+++ users/lcleveland/settings/niri.nix
@@ -1,12 +1,14 @@
 { config, lib, inputs, ... }:
 {
-  home-manager.users.lcleveland.programs.niri = {
-    enable = true;
-    settings = {
-      binds = {
-        "Mod+T".action.spawn = "ghostty";
+  config = {
+    home-manager.users.lcleveland.programs.niri = {
+      enable = true;
+      settings = {
+        binds = {
+          "Mod+T".action.spawn = "ghostty";
+        };
+        input.keyboard.xkb.variant = "colemak_dh";
       };
-      input.keyboard.xkb.variant = "colemak_dh";
     };
   };
 }
