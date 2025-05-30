b29b9cf4c5136c62f6ba70d35bab4b3f861493f3
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 19:00:15 -0500

    add kb variant

--- users/lcleveland/settings/niri.nix
+++ users/lcleveland/settings/niri.nix
@@ -3,8 +3,11 @@
   config = {
     home-manager.users.lcleveland.programs.niri = {
       enable = true;
-      settings.binds = {
-        "Mod+T".action.spawn = "ghostty";
+      settings = {
+        binds = {
+          "Mod+T".action.spawn = "ghostty";
+        };
+        input.keyboard.variant = "colemak_dh";
       };
     };
   };
