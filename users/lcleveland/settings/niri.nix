fdb1fb8be570dbed5aaaf42a757adcae3f388b87
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 19:22:33 -0500

    test

--- users/lcleveland/settings/niri.nix
+++ users/lcleveland/settings/niri.nix
@@ -1,2 +1,13 @@
 { config, lib, inputs, ... }:
-{ }
+{
+  config = {
+    programs.niri = {
+      settings = {
+        binds = {
+          "Mod+T".action.spawn = "ghostty";
+        };
+        input.keyboard.variant = "colemak_dh";
+      };
+    };
+  };
+}
