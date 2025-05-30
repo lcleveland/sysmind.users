eedf97138e3658fdb4f2195a92953769a46e51d9
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-30 10:30:55 -0500

    test waybar

---
+++ users/lcleveland/settings/waybar.nix
@@ -0,0 +1,14 @@
+{ config, lib, inputs, ... }:
+{
+  config = {
+    home-manager.users.lcleveland.programs = {
+      waybar = {
+        enable = true;
+        style = ''
+          * {
+            font-family: "Roboto Mono", "Font Awesome 6 Free Solid", "Font Awesome 6 Brands";
+        '';
+      };
+    };
+  };
+}
