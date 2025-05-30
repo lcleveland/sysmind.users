f5dccec2203ff6ab1cb70731658738a90f9b8b46
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 20:25:19 -0500

    add fuzzel

--- users/lcleveland/settings/niri.nix
+++ users/lcleveland/settings/niri.nix
@@ -1,14 +1,20 @@
 { config, lib, inputs, ... }:
 {
   config = {
-    home-manager.users.lcleveland.programs.niri = {
-      settings = {
-        binds = {
-          "Mod+T".action.spawn = "ghostty";
-        };
-        input.keyboard.xkb = {
-          layout = "us";
-          variant = "colemak_dh";
+    home-manager.users.lcleveland.programs = {
+      fuzzel = {
+        enable = true;
+      };
+      niri = {
+        settings = {
+          binds = {
+            "Mod+D".action.spawn = "fuzzel";
+            "Mod+T".action.spawn = "ghostty";
+          };
+          input.keyboard.xkb = {
+            layout = "us";
+            variant = "colemak_dh";
+          };
         };
       };
     };
